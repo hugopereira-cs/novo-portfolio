dc5683a447c8506e99ab88cc978f0ec74523e50e
hugopereira-cs <hugoper_cs@yahoo.com>
2026-01-08 16:00:19 -0300

    fix: profile.json error

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,28 @@ function updateProfileInfo(profileData) {
   photo.alt = `Foto de ${profileData.name}`
 
   const name = document.getElementById('profile.name')
-  name.textContent = profileData.name
+  name.innerText = profileData.name
+
+  const job = document.getElementById('profile.job')
+  job.innerText = profileData.job
+
+  const location = document.getElementById('profile.location')
+  location.innerText = profileData.location
+
+  const linkedin = document.getElementById('profile.linkedin')
+  linkedin.href = profileData.linkedin
+  linkedin.innerText = `LinkedIn`
+
+  const mail = document.getElementById('profile.mail')
+  mail.href = `mailto:${profileData.email}`
+  mail.innerText = profileData.email
+}
+
+function updateSkillsInfo(profileData) {
+  const softSkills = document.getElementById('profile.skills.softSkills')
+
+  // Mapeia cada soft skill para um elemento de lista HTML (<li>) e atualiza a seção.
+  softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
 
@@ -18,4 +39,5 @@ function updateProfileInfo(profileData) {
 (async() => {
   const profileData = await fetchProfileData()
   updateProfileInfo(profileData)
+  updateSkillsInfo(profileData)
 })()
